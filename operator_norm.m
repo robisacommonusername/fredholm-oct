63feb21480e3d41fa1e7d7c8ad74b4d4360b69c5
Robert Palmer <robisacommonusername@server.fake>
2014-06-03 14:50:55 +0800

    created tests for operator norm, and modified operator_norm to account for non equally spaced quadrature points

--- operator_norm.m
+++ operator_norm.m
@@ -1,35 +1,47 @@
 %find the largest singular value of discretised Fredholm operator K
-% and thus estimate the l2 norm
-function sqrt_sigma = operator_norm(Kd,varargin)
+% and thus estimate the L2 norm
+
+%x is defined at the quadrature points, so norm(x)
+%may not be a good estimate of |x|. In order to compute |x|
+%we need to know the quadrature weights, and |x| = sqrt(weights' * x.^2)
+function [sqrt_sigma, numIters] = operator_norm(Kd,weights,varargin)
 	%user can optionally specify a tolerance and max number of iterations,
 	%otherwise use default values
 	maxIters = 1000;
-	eps = 0.0001;
-	if nargin > 1
-		eps = varargin{1};
+	tol = 0.0001;
+	if nargin > 3
+		tol = varargin{1};
 	end;
-	if nargin > 2
+	if nargin > 4
 		maxIters = varargin{2};
 	end;
 	
 	KdagK = ctranspose(Kd)*Kd;
 	
 	[rows,cols] = size(KdagK);
+	%We expect that the singular function corresponding to the largest
+	%singular value will be the least oscillatory of the singular
+	%functions, hence we choose a constant x as our initial guess.
 	x = ones(cols,1);
-	x = x/norm(x);
-	diff = eps+1;
-	guard = 0;
+	x = x/sqrt(weights' * x.^2);
+	eps = tol+1;
+	numIters = 0;
 	xold = x;
 	
-	while (diff > eps) && (guard < maxIters)
+	while (eps > tol) && (numIters < maxIters)
 		x = KdagK*x;
-		x = x/norm(x);
-		diff = norm(x-xold);
+		x = x/sqrt(weights' * x.^2);
+		eps = sqrt(weights' * (x-xold).^2);
 		xold = x;
-		guard = guard + 1;
+		numIters = numIters + 1;
 	end;
 	
-	sqrt_sigma = sqrt(norm(KdagK*x));
+	y = KdagK*x;
+	%For complex matrices, there may be a small imaginary component
+	%which shouldn't be there (ie the iterations will terminate before
+	%the imaginary component can be reduced to zero). The call to abs
+	%will ensure that we always return a real number
+	sqrt_sigma = abs(weights' * y.^2)^0.25;
 	
 	
 end
---
+++ tests/test_operator_norm.m
@@ -0,0 +1,67 @@
+%Boilerplate
+function ret = test_operator_norm(varargin)
+	test_names = find_tests(mfilename());
+	tests={};
+	for ii = 1:length(test_names) 
+		tests(ii) = str2func(test_names(ii)); 
+	end;
+	ret = run_tests(tests, test_names);
+end;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%TEST FUNCTIONS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [status,msg] = test_real_L2_equiv()
+	%If we use [1;1;...] as the quadrature weights, we whould compute
+	%the same result as returned by norm(A)
+	results_testfn = zeros(50,1);
+	results_matlab = zeros(50,1);
+	for ii = 1:50
+		A = rand(5,5);
+		results_testfn(ii) = operator_norm(A,[1;1;1;1;1]);
+		results_matlab(ii) = norm(A);
+	end;
+	[status, msg] = assert_eq(results_testfn, results_matlab);
+end;
+
+function [status, msg] = test_complex_L2_equiv()
+	%same as previous test, but use complex matrices
+	results_testfn = zeros(50,1);
+	results_matlab = zeros(50,1);
+	for ii = 1:50
+		A = rand(5,5) + i*rand(5,5);
+		results_testfn(ii) = operator_norm(A,[1;1;1;1;1]);
+		results_matlab(ii) = norm(A);
+	end;
+	[status, msg] = assert_eq(results_testfn, results_matlab);
+end;
+
+function [status, msg] = test_alternative_quadratures()
+	%we should be able to use alternative quadratures, but get similar
+	%results for the operator norm
+	H = @(k,z) exp(-(k^2 + z^2));
+	[Kd1, pts1, weights1] = discretise_operator(H, 30, 'gauss10');
+	[Kd2, pts2, weights2] = discretise_operator(H, 51, 'simpson');
+	norm_gauss = operator_norm(Kd1,weights1);
+	norm_simps = operator_norm(Kd2,weights2);
+	[status, msg] = assert_eq(norm_gauss, norm_simps);
+end;
+
+function [status, msg] = test_convergence()
+	%ensure convergence when dominant eigenvalues are very close
+	tol = 0.0001;
+	maxIters = 1000;
+	for eps = [1 0.1 0.01 0.001]
+		A = diag([5+eps, 5-eps, 0.4]);
+		[norm, iters] = operator_norm(A,[1;1;1],tol,maxIters);
+		if iters >= maxIters
+			status = 1;
+			msg = sprintf('FAIL - did not converge for eps=%f', eps);
+			return;
+		end;
+	end;
+	status = 0;
+	msg = 'PASS';
+	
+end;
