b84dd7670bf1f1e00c75214958f8938de6842450
Robert Palmer <robisacommonusername@server.fake>
2014-06-15 22:49:09 +0800

    corrected gauss kernel function after dimensional analysis

--- gauss_kernel.m
+++ gauss_kernel.m
@@ -2,6 +2,6 @@
 %Returns an annonymous function for given Q, alpha, z0
 %Q is just a scalar here, as kernel only depends on $\|mathbf{Q}\|$
 function H = gauss_kernel(Q,alpha,z0)
-	H = @(k,z) 1/4/pi/(alpha^2/k^2 + i*(z-z0)/k)*exp(2*i*(z-z0)*k-Q^2/(alpha^2/k^2+i*(z-z0)/k));
+	H = @(k,z) 1/4/pi/(alpha^2/k^2 + i*(z-z0)/k)*exp(2*i*(z-z0)*k-Q^2/2*(alpha^2/2/k^2+i*(z-z0)/2/k));
 
 end
