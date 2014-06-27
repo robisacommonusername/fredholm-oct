05d52e66473434f9294acf18b38700e28bff9ac3
Robert Palmer <robisacommonusername@server.fake>
2014-06-27 20:55:56 +0800

    modified iterative solver to permit an x0 parameter

--- solve_iteratively.m
+++ solve_iteratively.m
@@ -1,11 +1,18 @@
 %Iterative solver for large 2nd kind Fredholm equation in form
-% KdagS = eps*x + Kdag*K*x
-% Where K is positive definite
+% KdagS = eps*(x-x0) + Kdag*K*x
+%
 function [chi, error, iterations] = solve_iteratively(Kd, Kdag, S, eps, weights, varargin)
-	%allow user to specify |K| to prevent recalculating it
+	%specify initial guess
 	if nargin > 5
-		if varargin{1} ~= 0
-			normK = varargin{1};
+		x0 = varargin{1};
+	else
+		x0 = zeros(length(S),1);
+	end;
+	
+	%allow user to specify |K| to prevent recalculating it
+	if nargin > 6
+		if varargin{2} ~= 0
+			normK = varargin{2};
 		else
 			normK = operator_norm(Kd,Kdag,weights);
 		end;
@@ -15,17 +22,17 @@ function [chi, error, iterations] = solve_iteratively(Kd, Kdag, S, eps, weights,
 	
 	%Default tolerance and maximum iterations
 	tol = 0.0001;
-	maxIters = 1000;
-	if nargin > 6
-		tol = varargin{2};
-	end;
+	maxIters = 10000;
 	if nargin > 7
-		maxIters = varargin{3};
+		tol = varargin{3};
+	end;
+	if nargin > 8
+		maxIters = varargin{4};
 	end;
 	
 	%select relaxation parameter. 0.5 is a safety factor
-	%transform equation into form $(I - \lambda \hat{K})\chi = y$
-	y = -1*(Kdag*S);
+	%transform equation into form $(lambdaI - \hat{K})\chi = y$
+	y = -1*(Kdag*S+eps*x0);
 	lambda = -1*eps;
 	mu = 0.5 * -2*lambda/(normK^2 - lambda);
 	
@@ -37,15 +44,16 @@ function [chi, error, iterations] = solve_iteratively(Kd, Kdag, S, eps, weights,
 	
 	iterations = 0;
 	error = abs_tol + 1;
-	%TODO: improve starting guess. Will converge correctly anyway, due
-	%to contraction mapping theorem (there can only be one fixed point)
 	chi = (mu/lambda)*y;
-
 	while error > abs_tol && iterations < maxIters
 		chi_old = chi;
 		chi = (mu/lambda)*y + (1-mu)*chi + Kdag*(Kd*((mu/lambda)*chi));
 		error = sqrt(weights' * (chi - chi_old).^2);
 		iterations = iterations + 1;
+	end;
+	
+	if iterations == maxIters
+		disp('WARNING: solve_iteratively returned after maximum number of iterations was exceeded');
 	end;
 	
 	error = error/error_multiplier;
