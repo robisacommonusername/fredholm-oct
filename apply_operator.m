1c7a1e428e4eab83afb992353eb773a17ae9c3d8
Robert Palmer <robisacommonusername@server.fake>
2014-06-28 23:37:00 +0800

    removed defunct apply operator

--- apply_operator.m
+++
@@ -1,5 +0,0 @@
-%Return function S which can be evaluated anywhere. Specify a function chi
-%and kernel H. Assume domain of integration is [0,1]
-function S = apply_operator(H,chi)
-	S = @(k) quad(@(z) H(k,z) * chi(z), 0, 1);
-end
