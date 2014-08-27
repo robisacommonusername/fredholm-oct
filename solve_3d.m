b80f49f8dd4a118b9ec329c1fee38d67e2fe2477
Robert Palmer <robisacommonusername@server.fake>
2014-08-27 16:42:23 +0800

    Issue 327 - optional 2d fourier transforms in solve_3d

--- solve_3d.m
+++ solve_3d.m
@@ -2,7 +2,7 @@
 %
 %Usage:
 %[chi, rx, ry, rz, error] = solve_3d(S, S_xi, S_yi, S_ki, f, A, A_ki,...
-%	pen_depth, varargin)
+%	zf, varargin)
 %
 %Input Parameters
 %	S: recorded interferometric data, S(x,y,k) in rank three tensor. 
@@ -21,9 +21,13 @@
 %	A: Source spectrum envelope
 %	A_ki: Sampling points in k domain of spectrum
 %
-%	pen_depth: expected beam penetration depth
+%	zf: scatterer thickness along z-axis
 %
 %	varargin:
+%		do_fft: By default set to 1, in which case we do the 2D fourier
+%		transforms. If set to 0, it will be assumed that S is already 
+%		dft, and S_xi and S_yi actually contain the frequencies
+%		
 %		opts_1d: additional options to pass to the 1d solver. Refer to 
 %		function solve_1d_opts for allowed options and parameters
 %
@@ -36,31 +40,46 @@
 %	error: not currently defined or used
 
 
-function [chi, rx, ry, rz, error] = solve_3d(S, S_xi, S_yi, S_ki, H, A, A_ki, pen_depth, varargin)
+function [chi, rx, ry, rz, error] = solve_3d(S, S_xi, S_yi, S_ki, f, A, A_ki, zf, varargin)
 	%get additional options for the 1d solver
 	if nargin > 8
-		opts_1d = varargin{1};
+		do_fft = varargin{1};
+	else
+		do_fft = 1;
+	end;
+	if nargin > 9
+		opts_1d = varargin{2};
 	else
 		opts_1d = solve_1d_opts(); %use defaults
 	end;
 	
 	%TODO: need to deal with non-uniform sampling in xy plane before fft
 	%perform fft on each slice of S
-	Stilde = fft2(S);
+	if do_fft
+		Stilde = fft2(S);
+		
+		%assume z axis passes through middle of object
+		x_width = S_xi(end) - S_xi(1);
+		y_width = S_yi(end) - S_yi(1);
+		B_x = 2*pi/x_width;
+		B_y = 2*pi/y_width;
+		N_x = length(S_xi);
+		N_y = length(S_yi);
+		%spatial frequencies from 0 to B/2 are in samples 1 to N/2.
+		%spatial frequencies from -B/2 to <0 are in samples N/2+1 to N
+		dQx = B_x/N_x;
+		dQy = B_y/N_y;
+		Qx = [0:dQx:B_x/2, -1*B_x/2:dQx:(-1*dQx)];
+		Qy = [0:dQy:B_y/2, -1*B_y/2:dQy:(-1*dQy)];
+	else
+		Stilde = S;
+		Qx = S_xi;
+		Qy = S_yi;
+	end;
 	
-	%assume z axis passes through middle of object
-	x_width = S_xi(end) - S_xi(1);
-	y_width = S_yi(end) - S_yi(1);
-	B_x = 2*pi/x_width;
-	B_y = 2*pi/y_width;
-	N_x = length(S_xi);
-	N_y = length(S_yi);
-	%spatial frequencies from 0 to B/2 are in samples 1 to N/2.
-	%spatial frequencies from -B/2 to <0 are in samples N/2+1 to N
-	dQx = B_x/N_x;
-	dQy = B_y/N_y;
-	Qx = [0:dQx:B_x/2, -1*B_x/2:dQx:(-1*dQx)];
-	Qy = [0:dQy:B_y/2, -1*B_y/2:dQy:(-1*dQy)];
+	%We reshape the array S now to get the z axis as the major dimension
+	%This is to optimise memory access
+	Stilde = shiftdim(Stilde,1);
 
 	[ix_max, iy_max, npoints] = size(Stilde);
 	z_pts = [];
@@ -68,13 +87,14 @@ function [chi, rx, ry, rz, error] = solve_3d(S, S_xi, S_yi, S_ki, H, A, A_ki, pe
 	for ix = 1:ix_max
 		for iy = 1:iy_max
 			f1d = f(Qx(ix), Qy(iy));
-			[chiz, z_pts, error] = solve_1d(f1d, Stilde(ix,iy,:), S_ki,...
-				A, A_ki, pen_depth, opts_1d );
+			[chiz, z_pts, error] = solve_1d(f1d, Stilde(:,ix,iy), S_ki,...
+				A, A_ki, zf, opts_1d );
 			Stilde(ix,iy,:) = chiz; %reuse memory
 		end;
 	end;
 	
-	%perform ifft on each slice
+	%perform ifft on each slice. Shift back to optimise memory access pattern
+	Stilde = shiftdim(Stilde,-1);
 	chi = ifft2(Stilde);
 	rx = S_xi;
 	ry = S_yi;
