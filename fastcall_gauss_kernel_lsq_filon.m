e942ab670a8eab7b05207610ac51599e0b62389f
Robert Palmer <robisacommonusername@server.fake>
2014-10-03 16:29:47 +0800

    correction to filon gauss kernel, added a todo to galerkinise kernel

--- fastcall_gauss_kernel_lsq_filon.m
+++ fastcall_gauss_kernel_lsq_filon.m
@@ -16,14 +16,14 @@ function [Kd,Kdag,pts,weights] = fastcall_gauss_worker(A,ki,zf,Q,alpha,z0,opts)
 		n = opts.n;
 	end;
 	
-	[pts, weights] = filon_quadrature(n, ki);
+	[pts, weights] = filon_quadrature(n, 2*ki);
 	
 	z_quad = unwarp_data(opts.warp_method, pts, zf);
 	deriv = warp_derivative(opts.warp_method, pts, zf);
 	
 	[zz,kk] = meshgrid(z_quad, ki); %z increases across row, k decreases down column
 	common_term = ((alpha^2)./kk + i*(zz-z0))./kk;
-	K_unweighted = 0.5*i*diag(A)*((1./common_term).*exp(2*i*(zz-z0).*kk-Q^2/4*common_term))*diag(deriv);
+	K_unweighted = 0.5*i*diag(A)*((1./common_term).*exp(-2*i*z0*kk).*exp(-1*Q^2/4*common_term))*diag(deriv);
 	Kd = K_unweighted.*weights;
 	Kdag = ctranspose(Kd); %This doesn't have a direct interpretation in
 	%terms of the integral operator, it's the adjoint of the discretised
--- galerkinise_kernel.m
+++ galerkinise_kernel.m
@@ -1,6 +1,8 @@
 %Convert the kernel described by the function H into a frequency domain
 %kernel by discretising with n point filon quadrature. n is number of
 %quad points required to integrate H(z) by simpsons rule
+
+%TODO: what if H itself contains an oscillatory factor??
 function [Kd, Kdag, pts, weights] = galerkinise_kernel(H, ki, zf, n, wc)
 	%We need to compute \int_0^zf H(k,z) e^iwz dz
 	%For every k in ki, and every w in [-wc/2, wc/2]
@@ -9,9 +11,11 @@ function [Kd, Kdag, pts, weights] = galerkinise_kernel(H, ki, zf, n, wc)
 	if mod(N,2) == 0
 		N = N+1;
 	end;
-	zeta = linspace(0,1,N);
+	Ts = 1/N;
+	w = 2*pi*[0:((N-1)/2), ((1-N)/2):-1]';
+	
+	zeta = linspace(0,1,n);
 	z = unwarp_data('linear',zeta, zf);
-	w = 2*pi/N*[0:((N-1)/2), ((1-N)/2):-1]';
 	[pts, weights] = filon_quadrature(n, w);
 	%weights is z across row, w down column
 	[zz,kk] = meshgrid(z,k); %z across row, k down column
