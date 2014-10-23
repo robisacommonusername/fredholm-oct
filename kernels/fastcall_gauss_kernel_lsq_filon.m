a49eba65306bfc0382c3f9a43c9d76a0f3a424ce
Robert Palmer <robisacommonusername@server.fake>
2014-10-23 17:22:53 +0800

    return proper weights and points from fastcall function

--- kernels/fastcall_gauss_kernel_lsq_filon.m
+++ kernels/fastcall_gauss_kernel_lsq_filon.m
@@ -16,15 +16,20 @@ function [Kd,Kdag,pts,weights] = fastcall_gauss_worker(A,ki,zf,Q,alpha,z0,opts)
 		n = opts.n;
 	end;
 	
-	[pts, weights] = filon_quadrature(n, 2*ki);
+	%pts and weights for the kernel discretisation
+	[pts_filon, weights_filon] = filon_quadrature(n, 2*ki);
+	%pts and weights for calculating function norms - essentially just
+	%a simpson quadrature, assumed that all the oscillation is in the kernel,
+	%not in chi
+	[pts, weights] = generate_quadrature('filon',n);
 	
-	z_quad = unwarp_data(opts.warp_method, pts, zf);
-	deriv = warp_derivative(opts.warp_method, pts, zf);
+	z_quad = unwarp_data(opts.warp_method, pts_filon, zf);
+	deriv = warp_derivative(opts.warp_method, pts_filon, zf);
 	
 	[zz,kk] = meshgrid(z_quad, ki); %z increases across row, k decreases down column
 	common_term = ((alpha^2)./kk + i*(zz-z0))./kk;
 	K_unweighted = 0.5*i*diag(A)*((1./common_term).*exp(-2*i*z0*kk).*exp(-1*Q^2/4*common_term))*diag(deriv);
-	Kd = K_unweighted.*weights;
+	Kd = K_unweighted.*weights_filon;
 	Kdag = ctranspose(Kd); %This doesn't have a direct interpretation in
 	%terms of the integral operator, it's the adjoint of the discretised
 	%operator only.
